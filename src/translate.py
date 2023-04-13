a2ed4014f302d9ead69eeabbcf96780efd0f0970
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-04-13 10:01:16 -0400

    Update translate.py

--- src/translate.py
+++ src/translate.py
@@ -1,5 +1,22 @@
 from typing import Optional
 def translate(json_data: dict, *, separator = ";", file = None, file_and_return = False) -> Optional[str]:
+  """
+  # translate
+  ```python
+  translate(json_data: dict, *, separator = ";", file = None, file_and_return = False) ->   Optional[str]
+  A small and simple json to csv module with custom separators
+  ```
+  This function translate some json data into a csv data and returns it in a file or in a string
+  ## json_data
+  The json data that will turned some json data into a csv string
+  ## separator
+  The separator for the csv string (normaly ";", "," or " ")
+  ## file
+  the file that the data will be written to (optional)
+  ## file_and_return
+  if there is a file, does it return the result
+
+  """
   lines = ["", ""]
   done = 0
   for i, j in json_data.items():
